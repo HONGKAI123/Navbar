22a6d67c97574e9b719766054e2e106581494576
HONGKAI123 <huanghongkai98@GMAIL.COM>
2024-05-12 16:43:41 -0700

    create dropdown menu with hide/show logic

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+interface DropDownMenuProps {
+  dropdownItems: string[];
+  isDropDownActive: boolean;
+}
 
-function DropdownMenu() {
-  return <div>DropdownMenu</div>;
+function DropdownMenu({ dropdownItems, isDropDownActive }: DropDownMenuProps) {
+  if (!dropdownItems) return null;
+
+  return (
+    <div className="dropdown-container">
+      <div className={`dropdown-menu ${isDropDownActive ? "active" : ""}`}>
+        {dropdownItems.map((item, index) => {
+          return (
+            <a className="dropdown-link" href="#dropDown" key={index}>
+              {item}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default DropdownMenu;
