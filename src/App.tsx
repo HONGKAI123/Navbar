74b0e66ab30fb4e843b75363202cab02bdabe1d8
HONGKAI123 <huanghongkai98@GMAIL.COM>
2024-05-12 19:38:53 -0700

    update background image for PC version

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="bgdiv">
+        <img
+          alt=""
+          src={`${process.env.PUBLIC_URL}/home.jpg`}
+          className="animatedbgimg"
+          role="banner"
+        />
+      </div>
+      <h1 className="animatedbigtitle">Enjoy Your Coding Journey! </h1>
     </div>
   );
 }
