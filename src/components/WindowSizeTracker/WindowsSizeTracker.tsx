3bac06321024d9820ada5f5c817311fb89eeaadc
HONGKAI123 <huanghongkai98@GMAIL.COM>
2024-05-12 17:41:02 -0700

    add sizeTracker to test and improve responsive design

---
+++ src/components/WindowSizeTracker/WindowsSizeTracker.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+function WindowSizeTracker() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  console.log("current size:", windowWidth);
+
+  return (
+    <div>
+      <p>Current size: {windowWidth}px</p>
+    </div>
+  );
+}
+
+export default WindowSizeTracker;
